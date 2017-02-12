28fdcaa39d814564092966c918c726dba8dd5bd9
CSC 230 Student <student@centos66.(none)>
2017-02-11 22:36:21 -0500

    Updated about page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,7 +14,8 @@ export default class About extends Component {
     return (
       <div className={classnames('About', className)} {...props}>
         <h1>
-          About
+          About:
+          The time has come, America, to fight back against the fake news sites of the world, such as the <b>notorious</b> CNN. This platform is designed to bring you the real news. The best news. And we think that it will affect the media <em><b>BIGLY</b></em>. We think people are tired of the lying media; I hear everyday that "something needs to be done" and "you all are the best for the job." After all, they did elect us. They know we have great information, the best information, perfect, like it's crazy. They love us. And they will <em>love</em> this too.
         </h1>
       </div>
     );
